c1a1d245baa7422edc805e9a34fc1d0de117d874
jiangnana.jnn <jiangnana.jnn@alibaba-inc.com>
2026-03-19 14:35:10 +0800

    update vla_pipeline

--- demos/ego_hand_action_annotation/vla_pipeline.py
+++ demos/ego_hand_action_annotation/vla_pipeline.py
@@ -1,5 +1,4 @@
 import os
-import json
 import sys
 sys.path.insert(0, os.path.dirname(__file__))
 
@@ -15,27 +14,6 @@ from data_juicer.ops.mapper import (
     ExportToLeRobotMapper)
 
 from custom_ops.video_action_captioning_mapper import VideoActionCaptioningMapper
-
-
-def save_to_jsonl(samples, output_file):
-    total_samples = len(samples[video_key])
-    all_keys = list(samples.keys())
-    
-    all_samples = []
-    for sample_idx in range(total_samples):
-        sample = {}
-        for k in all_keys:
-            sample[k] = samples[k][sample_idx]
-        all_samples.append(sample)
-    
-    try:
-        with open(output_file, 'w') as f:
-            lines= [json.dumps(sample) for sample in all_samples]
-            f.write('\n'.join(lines))
-    except Exception as e:
-        print(e)
-
-    return all_samples
 
 
 if __name__ == '__main__':
@@ -81,8 +59,8 @@ if __name__ == '__main__':
     )
 
     action_op = VideoHandActionComputeMapper(
-        hand_reconstruction_field=MetaKeys.hand_reconstruction_hawor_tags,
-        camera_pose_field=MetaKeys.video_camera_pose_tags,
+        hand_reconstruction_field=MetaKeys.hand_reconstruction_hawor_tags,  # outputs of VideoHandReconstructionHaworMapper
+        camera_pose_field=MetaKeys.video_camera_pose_tags,  # outputs of VideoCameraPoseMegaSaMMapper
         tag_field_name=MetaKeys.hand_action_tags,
         hand_type="both",
         batch_mode=True,
@@ -100,7 +78,7 @@ if __name__ == '__main__':
 
     export_op = ExportToLeRobotMapper(
         output_dir=LEROBOT_OUTPUT_DIR,
-        hand_action_field=MetaKeys.hand_action_tags,
+        hand_action_field=MetaKeys.hand_action_tags,    # outputs of VideoHandActionComputeMapper
         frame_field=MetaKeys.video_frames,
         video_key=video_key,
         task_description_key="text",
@@ -130,7 +108,7 @@ if __name__ == '__main__':
             batch_size=1,
             num_gpus=0.1,
             batch_format="pyarrow",
-            compute=ActorPoolStrategy(min_size=1, max_size=2),
+            compute=ActorPoolStrategy(min_size=1, max_size=2),  # adjust the scope based on available resources
             runtime_env=None,
         ).map_batches(
             VideoHandReconstructionHaworMapper,
@@ -146,7 +124,7 @@ if __name__ == '__main__':
             batch_size=1,
             num_gpus=0.1,
             batch_format="pyarrow",
-            compute=ActorPoolStrategy(min_size=1, max_size=2),
+            compute=ActorPoolStrategy(min_size=1, max_size=2),  # adjust the scope based on available resources
             runtime_env=None,
         ).map_batches(
             VideoCameraPoseMegaSaMMapper,
@@ -159,7 +137,7 @@ if __name__ == '__main__':
             batch_size=1,
             num_gpus=0.1,
             batch_format="pyarrow",
-            compute=ActorPoolStrategy(min_size=1, max_size=2),
+            compute=ActorPoolStrategy(min_size=1, max_size=2),  # adjust the scope based on available resources
             runtime_env={"conda": "mega-sam"},
         ).map_batches(
             action_op,
@@ -170,13 +148,12 @@ if __name__ == '__main__':
         ).map_batches(
             caption_op,
             batch_size=1,
-            num_cpus=2,
             batch_format="pyarrow",
             runtime_env=None,
         ).map_batches(
             export_op,
             batch_size=1,
-            num_cpus=2,
+            num_cpus=1,
             batch_format="pyarrow",
             runtime_env=None,
         )
